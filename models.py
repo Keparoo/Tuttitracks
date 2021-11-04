ed3b45df354c09aaaa4c067f9c3a66f36ca11eb1
Kep <KepCreations@gmail.com>
2021-11-04 14:52:53 -0400

    Create tabel models

--- models.py
+++ models.py
@@ -30,13 +30,16 @@ def connect_db(app):
     print(f'Connecting to: {database_path}')
 
 class User(db.Model):
-    """Model for a User class"""
+    """Model for User class"""
 
     __tablename__ = 'users'
 
-    username = db.Column(db.String(25), primary_key=True)
-    password = db.Column(db.Text, nullable=False)
-    email = db.Column(db.String(50), nullable=False)
+    username = db.Column(db.Text(25), primary_key=True)
+    password = db.Column(db.String, nullable=False)
+    email = db.Column(db.Text(50), nullable=False)
+    spotify_user_id = db.Column(db.Text)
+    spotify_display_name = db.Column(db.Text)
+    user_image = db.Column(db.String)
     is_admin = db.Column(db.Boolean, nullable = False, default=False)
     current_market = db.Column(db.String(2), default='US')
 
@@ -101,3 +104,112 @@ class User(db.Model):
 
         db.session.delete(user)
         db.session.commit()
+
+class Track(db.Model):
+    """Model for music track class"""
+
+    __tablename__ = 'tracks'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    spotify_track_id = db.Column(db.String, nullable=False)
+    name = db.Column(db.String, nullable=False)
+    spotify_track_url = db.Column(db.String, nullable=False)
+    spotify_track_uri = db.Column(db.String, nullable=False)
+    is_playable = db.Column(db.Boolean, nullable=False)
+    preview_url = db.Column(db.String) # can be null
+    release_year = db.Column(db.Integer, nullable=False)
+    popularity = db.Column(db.Integer) # (0-100)
+    duration = db.Column(db.Integer, nullable=False)
+
+    acousticness = db.Column(db.Float) # (0.0-1.0)
+    danceability = db.Column(db.Float) # (0.0-1.0)
+    energy = db.Column(db.Float) # (0.0-1.0)
+    instrumentalness = db.Column(db.Float) # (0.0-1.0)
+    liveness = db.Column(db.Float) # (0.0-1.0)
+    loudness = db.Column(db.Float) # (0.0-1.0)
+    speechiness = db.Column(db.Float) # (0.0-1.0)
+    valence = db.Column(db.Float) # (0.0-1.0)
+    mode = db.Column(db.Integer) # (0=minor, 1=major)
+    key = db.Column(db.Integer) # (0-11: 0=C, 1=D-flat/C-sharp, 11=B)
+    time_signature = db.Column(db.Integer) # (number beats/measure)
+
+    lyrics = db.Column(db.String)
+    lyrics_language = db.Column(db.Text(2))
+
+class Playlist(db.Model):
+    """Model for music track playlist class"""
+
+    __tablename__ = 'playlists'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.username'), nullable=False)
+    spotify_playlist_id = db.Column(db.String)
+    image = db.Column(db.String)
+    public = db.Column(db.Boolean, default=True)
+    name = db.Column(db.String, nullable=False)
+    description = db.Column(db.String)
+
+class PlaylistTrack(db.Model):
+    """Model joins playlists to tracks"""
+
+    __tablename__ = 'playlists_tracks'
+
+    # Playlists can have the same track in multiple slots
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    playlist_id = db.Column(db.Integer, db.ForeignKey('playlists.id'), nullable=False)
+    track_id = db.Column(db.Integer, db.ForeignKey('tracks.id'), nullable=False)
+    # Playlists need an order
+    index = db.Column(db.Integer, nullable=False)
+
+class Album(db.Model):
+    """Model of music albums"""
+
+    __tablename__ = 'albums'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    spotify_album_id = db.Column(db.String, nullable=False)
+    name = db.Column(db.String, nullable=False)
+    image = db.Column(db.String, nullable=False)
+    image_height = db.Column(db.Integer, nullable=False) # may not be needed
+    image_width = db.Column(db.Integer, nullable=False) # may not be needed
+
+class TrackAlbum(db.Model):
+    """Model joins tracks to albums"""
+
+    __tablename__ = 'tracks_albums'
+
+    track_id = db.Column(db.Integer, db.ForeignKey("tracks.id"), primary_key=True)
+    album_id = db.Column(db.Integer, db.ForeignKey("albums.id"), primary_key=True)
+
+class Artist(db.Model):
+    """Model of music artists"""
+
+    __tablename__ = 'artists'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    spotify_artist_id = db.Column(db.String, nullable=False)
+    name = db.Column(db.String, nullable=False)
+
+class TrackArtist(db.Model):
+    """Model joins tracks to artists"""
+
+    __tablename__ = 'tracks_artists'
+
+    track_id = db.Column(db.Integer, db.ForeignKey("tracks.id"), primary_key=True)
+    artist_id = db.Column(db.Integer, db.ForeignKey("artists.id"), primary_key=True)
+
+class Genre(db.Model):
+    """Model of music genres"""
+
+    __tablename__ = 'genres'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String, nullable=False)
+
+class TrackGenre(db.Model):
+    """Model joins tracks to genres"""
+
+    __tablename__ = 'tracks_genres'
+
+    track_id = db.Column(db.Integer, db.ForeignKey("tracks.id"), primary_key=True)
+    genre_id = db.Column(db.Integer, db.ForeignKey("genres.id"), primary_key=True)
